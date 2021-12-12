000b420b4b272ba8e881adc2564cee1f2566d704
Olga Shvets <olgashvez84@gmail.com>
2021-12-12 20:45:58 +0200

    refactor script.js

--- examples/js-clock/script.js
+++ examples/js-clock/script.js
@@ -17,12 +17,12 @@ const time = document.querySelector('.time')
  let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
  let monthes = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
  
-    hour = today.getHours(),
+    let hour = today.getHours(),
     min = today.getMinutes(),
-    sec = today.getSeconds();
-    day = days[today.getDay()];
-    date =today.getDate();
-    month = monthes[today.getMonth()];
+    sec = today.getSeconds(),
+    day = days[today.getDay()],
+    date =today.getDate(),
+    month = monthes[today.getMonth()],
     year = today.getFullYear();
 
 time.innerHTML = `${addZero(hour)}:${addZero(min)}:${addZero(sec)} ${addZero(day)},  ${addZero(date)} ${addZero(month)} ${addZero(year)}`;
