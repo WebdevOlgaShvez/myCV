9987173acac59f8842830a51163e085a3f455dca
Olga Shvets <olgashvez84@gmail.com>
2021-12-12 20:48:28 +0200

    refactor script.js

--- examples/js-clock/script.js
+++ examples/js-clock/script.js
@@ -1,11 +1,11 @@
 setInterval(setClock, 1000);
 
- const hourHand = document.querySelector('[data-hour-hand]');
+const hourHand = document.querySelector('[data-hour-hand]');
 const minuteHand = document.querySelector('[data-minute-hand]'); 
 const secondHand = document.querySelector('[data-second-hand]'); 
 const time = document.querySelector('.time')
 
- function setClock() {
+function setClock() {
     const today= new Date();
     const seconds = (today.getSeconds()/60);
     const minutes = (today.getMinutes()/60);
@@ -17,13 +17,13 @@ const time = document.querySelector('.time')
  let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
  let monthes = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
  
-    hour = today.getHours(),
-    min = today.getMinutes(),
-    sec = today.getSeconds();
-    day = days[today.getDay()];
-    date =today.getDate();
-    month = monthes[today.getMonth()];
-    year = today.getFullYear();
+  let hour = today.getHours(),
+      min = today.getMinutes(),
+      sec = today.getSeconds(),
+      day = days[today.getDay()],
+      date =today.getDate(),
+      month = monthes[today.getMonth()],
+      year = today.getFullYear();
 
 time.innerHTML = `${addZero(hour)}:${addZero(min)}:${addZero(sec)} ${addZero(day)},  ${addZero(date)} ${addZero(month)} ${addZero(year)}`;
  
@@ -39,6 +39,7 @@ function addZero(n) {
 } 
 
 setClock();
+
 //dark mode
 document.querySelector('.darkmode').addEventListener('click', (event) => {
    event.preventDefault();
